d205ff4daceaabb854fbdf749f35a27a08eb3cec
AndreasGP <aaatttsss@hotmail.com>
2015-03-28 13:55:38 +0200

    User now has to choose an area before being able to log in for the first time.

--- valimised/application/controllers/logged_controller.php
+++ valimised/application/controllers/logged_controller.php
@@ -13,25 +13,56 @@ class Logged_Controller extends CI_Controller {
         $this->load->library('facebook');
     }
 
-    
     public function redirect($redirectURL) {
-        
+
         $data = array(
             //Title of the page
             "title" => "Sisse logitud",
         );
-                
-        $event = "logged";
+
         $this->load->helper('url');
         $this->load->library('facebook');
-        $this->load->view('templates/header.php', $data);      
-        $this->load->view('templates/navbar.php', $this->facebook->getLoginData());
-        $this->load->view('home', array("event" =>$event));
+        $this->load->view('templates/header.php', $data);
+        $fbdata = $this->facebook->getLoginData();
+        $this->load->view('templates/navbar.php', $fbdata);
+
+        if ($fbdata['user_profile']['verified'] == true) {
+            $userQuery = $this->db->select("*")->from("user")->where("email", $fbdata['user_profile']['email'])->get();
+
+            if ($userQuery->num_rows() === 1) {
+               $this->load->view('home', array("event" => "logged"));
+               header("refresh:3;url=http://morsakabi.planet.ee/valimised/" . "$redirectURL");
+            } else {
+                $selectedArea = $this->input->get("area");
+                
+                if ($selectedArea == 0) {
+                    //No area was selected
+                    $this->load->library("area_factory");
+                    $this->load->view('home', array("event" => "firstlogin",
+                        "redirectURL" => $redirectURL,
+                        "areas" => $this->area_factory->getArea()));
+                } else {
+                    //user has selected an area
+                    //Create a user
+                    $userdata = array(
+                        'id' => ('NULL'),
+                        'areaid' => $selectedArea,
+                        'firstname' => $fbdata['user_profile']['first_name'],
+                        'lastname' => $fbdata['user_profile']['last_name'],
+                        'email' => $fbdata['user_profile']['email']
+                    );
+                    $this->db->insert('user', $userdata);
+                    $this->load->view('home', array("event" => "logged"));
+                    header("refresh:3;url=http://morsakabi.planet.ee/valimised/" . "$redirectURL");
+                }
+            }
+
+
+        } else {
+            //User not properly logged in, alternative behaviour
+            $this->load->view('home', array("event" => "fail"));
+        }
         $this->load->view('templates/footer.php');
-
-        header( "refresh:3;url=http://morsakabi.planet.ee/valimised/"."$redirectURL" );
-        //redirect($redirectURL, 'refresh');
     }
-
 
 }
