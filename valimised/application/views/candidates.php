ca69a6277c69167b2fbdd769ba1a8f32dac245b4
markotaht <markotaht@gmail.com>
2015-03-28 13:53:44 +0200

    Kandidaatide otsing

--- valimised/application/views/candidates.php
+++ valimised/application/views/candidates.php
@@ -13,9 +13,9 @@
             <form role="form">
                 <div class="form-group">
                     <label for="name">Nime järgi:</label>
-                    <input type="text" class="form-control" id="name">
+                    <input type="text" class="form-control" ng-model="name" id="name">
                     <label for="pwd">Erakonna järgi:</label>
-                    <select class="form-control" id="party">
+                    <select class="form-control" ng-model="party" id="party" onchange="onPartyChange()">
                         <option value="" style="display:none;"></option>
                         <?php foreach ($parties as $row): ?>
                             <option><?php echo $row->getName(); ?></option>
@@ -29,7 +29,7 @@
         <div class="col-sm-8">
             <div ng-init="areaid = '<?php echo $areaid ?>'" ng-controller="CandidatesCtrl">
                 <table ng-table="tableParams" class="table table-striped">
-                    <tr ng-repeat="candidate in $data" ng-click="candidatePage({{candidate.id}})">
+                    <tr ng-repeat="candidate in $data | filter:name | filter:party " ng-click="candidatePage({{candidate.id}})">
                         <td class="col-md-1" data-title="'Kandidaadi number'" sortable="id">{{candidate.id}}</td>
                         <td class="col-md-3" data-title="'Nimi'" sortable="'name'"><a href='/valimised/kandidaat/nr/{{candidate.id}}'>{{candidate.firstname}} {{candidate.lastname}}</a></td>
                         <td class="col-md-4" data-title="'Kandideerib erakonnas'" sortable="'party'"><a href='/valimised/kandidaat/nr/{{candidate.id}}'>{{candidate.party}}</a></td>
